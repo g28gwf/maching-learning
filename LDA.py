8402174a8ed6b4a1395178c046ddaa8a8fb3d32f
g28gwf <1095208012@qq.com>
2018-10-22 14:29:14 +0800

    Update LDA.py

--- LDA.py
+++ LDA.py
@@ -6,33 +6,29 @@ import matplotlib.pyplot as plt
 inputfile='xigua3.0.xlsx'
 
 data = pd.read_excel(inputfile)
-x = data.values[:,0:2]
-y = data.values[:,2]
 dataset0 = np.array([list(data[u'密度']),list(data[u'含糖率']),list(data[u'好瓜'])])
 dataset0 = dataset0.T
 dataset = dataset0[:, 0: 2]
 is_good = dataset0[:, 2]
 u = []
 for i in range(2):
-    u.append(np.mean(x[y == i],axis = 0)) 
-m,n = np.shape(x)
+    u.append(np.mean(dataset[is_good == i],axis = 0)) 
+m,n = np.shape(dataset)
 sw = np.zeros((n,n))
 for i in range(m):
-    x_tmp = x[i].reshape(n,1)  #将x的每行数据转为n行1列
-    if y[i] == 0:
+    x_tmp = dataset[i].reshape(n,1)
+    if is_good[i] == 0:
         u_tmp = u[0].reshape(n, 1)
-    if y[i] == 1:
+    if is_good[i] == 1:
         u_tmp = u[1].reshape(n, 1)
     sw += np.dot(x_tmp - u_tmp,(x_tmp - u_tmp).T)
-sw = np.mat(sw) #转为矩阵
+sw = np.mat(sw)
 u,sigma,v = np.linalg.svd(sw)
 sw_inv = v.T * np.linalg.inv(np.diag(sigma)) * u.T
 w = np.dot(sw_inv, (u[0] - u[1]).reshape(n, 1))
 f = plt.figure(1)
 D = np.linspace(0.2, 0.8, 1000)
 a = w.A
-print(a)
-print(a[0])
 S = -1 * (D * a[0]) * (1 / a[1])
 plt.plot(D, S)
 #print(w[0])
